b43ea3f5951bab379f2ab16d188441721a7cbf76
Tim Bang <tim@suellner.de>
2018-12-10 22:53:10 +0100

    First Prototype

--- NetChat/NetChat.Front/Optionen.cs
+++ NetChat/NetChat.Front/Optionen.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace NetChat.Front
 {
-    public partial class Form1 : Form
+    public partial class Optionen : Form
     {
-        public Form1()
+        public Optionen()
         {
             InitializeComponent();
         }
