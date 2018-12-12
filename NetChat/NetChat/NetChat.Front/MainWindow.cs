eb953a0dcf932a8efc522540df896421bdbbacee
MrDariusAD <mrdariusad@gmail.com>
2018-12-12 23:57:15 +0100

    give MainWindow reference to updater Thread to prevent Exception

--- NetChat/NetChat/NetChat.Front/MainWindow.cs
+++ NetChat/NetChat/NetChat.Front/MainWindow.cs
@@ -71,19 +71,20 @@ namespace NetChat.Front
             _connection = new NetChatConnection(GlobalVariable.IP, GlobalVariable.Port, GlobalVariable.UserName);
             if (_connection.SocketIsNull())
                 _connection = null;
-            ChatUpdater = new Thread(updater);
-            ChatUpdater.Start();
+            ChatUpdater = new Thread(new ParameterizedThreadStart(updater));
+            ChatUpdater.Start(this);
         }
 
-        private void updater()
+        private void updater(object window)
         {
+            var mainWinow = window as MainWindow;
             while (KeepUpdating)
             {
                 Thread.Sleep(100);
                 foreach (Client.Core.Message m in _connection.RecievedMessages)
                 {
-                    Chat.Items.Add($"[{m.Username}] {m.Content}");
-                    Chat.SelectedIndex = Chat.Items.Count - 1;
+                    mainWinow.Chat.Items.Add($"[{m.Username}] {m.Content}");
+                    mainWinow.Chat.SelectedIndex = Chat.Items.Count - 1;
                 }
                 _connection.RecievedMessages.Clear();
             }
